b6ebd4b1965389ff23ec2689109ec5ea7d9cd725
unknown <@ngknn.local>
2024-02-07 13:49:40 +0300

    metod 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ВходнойКонтроль
 {
     class Control
     {
-        private static bool Closed(int[,] Array)
+        static bool Closed(int[,] Array)
         {
             int SumRow = 0;
             int SumColumn = 0;
@@ -35,8 +35,7 @@ namespace ВходнойКонтроль
                 return false;
             }
         }
-
-        private static int max(int[,] Array)
+        static int max(int[,] Array)
         {
             int max = Array[1, 1];
             for (int i = 1; i < Array.GetLength(0); i++)
@@ -51,8 +50,168 @@ namespace ВходнойКонтроль
             }
             return max;
         }
+        static void Metod1()
+        {
+            int N, M;
+            Console.WriteLine("Введите количество склада:");
+            N = Convert.ToInt32(Console.ReadLine()) + 1;
+            Console.WriteLine("Введите количество потреблений:");
+            M = Convert.ToInt32(Console.ReadLine()) + 1;
 
-        static void Main()
+            int[,] Tariff = new int[N, M];
+            int[,] Plan = new int[N, M];
+            int[,] Check = new int[N, M];
+            int SumCheck = 0; // 0 - не заполнен тариф, 1 - заполнен тариф
+            int L = 0; //Целевая функция
+            int V = (M - 1) + (N - 1) - 1; //Вырожденность
+            int countV = 0;
+
+            //Строки
+            Tariff[0, 1] = 13;
+            Tariff[0, 2] = 5;
+            Tariff[0, 3] = 13;
+            Tariff[0, 4] = 12;
+            Tariff[0, 5] = 13;
+            //Столбцы
+            Tariff[1, 0] = 14;
+            Tariff[2, 0] = 14;
+            Tariff[3, 0] = 14;
+            Tariff[4, 0] = 14;
+
+            //Тарифы
+            Tariff[1, 1] = 16;
+            Tariff[1, 2] = 26;
+            Tariff[1, 3] = 12;
+            Tariff[1, 4] = 24;
+            Tariff[1, 5] = 3;
+
+            Tariff[2, 1] = 5;
+            Tariff[2, 2] = 2;
+            Tariff[2, 3] = 19;
+            Tariff[2, 4] = 27;
+            Tariff[2, 5] = 2;
+
+            Tariff[3, 1] = 29;
+            Tariff[3, 2] = 23;
+            Tariff[3, 3] = 25;
+            Tariff[3, 4] = 16;
+            Tariff[3, 5] = 8;
+
+            Tariff[4, 1] = 2;
+            Tariff[4, 2] = 25;
+            Tariff[4, 3] = 14;
+            Tariff[4, 4] = 15;
+            Tariff[4, 5] = 21;
+
+
+            //Ввод
+            //Console.WriteLine("Введите склад:");  //Строки
+            //for (int i = 1; i < Tariff.GetLength(1); i++)
+            //{
+            //    Tariff[0, i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Введите потребления:"); //Столбцы
+            //for (int i = 1; i < Tariff.GetLength(0); i++)
+            //{
+            //    Tariff[i, 0] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Введите тарифы:");
+            //for (int i = 1; i < Tariff.GetLength(0); i++)
+            //{
+            //    for (int j = 1; j < Tariff.GetLength(1); j++)
+            //    {
+            //        Tariff[i, j] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            //Подсчет тарифного плана
+            if (Closed(Tariff))
+            {
+                Console.WriteLine("Тарифы:");
+                for (int i = 0; i < Tariff.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Tariff.GetLength(1); j++)
+                    {
+                        Console.Write(Tariff[i, j] + " \t");
+                    }
+                    Console.WriteLine();
+                }
+                while (SumCheck < (N - 1) * (M - 1))
+                {
+                    //Поиск минимального элемента
+                    int minEl = max(Tariff);
+                    for (int i = 1; i < Tariff.GetLength(0); i++)
+                    {
+                        for (int j = 1; j < Tariff.GetLength(1); j++)
+                        {
+                            if (Tariff[i, j] < minEl && Check[i, j] == 0)
+                            {
+                                minEl = Tariff[i, j];
+                            }
+                        }
+                    }
+                    //Заполняем ячейку тарифом
+                    for (int i = 1; i < Tariff.GetLength(0); i++)
+                    {
+                        for (int j = 1; j < Tariff.GetLength(1); j++)
+                        {
+                            if (Tariff[i, j] == minEl && Check[i, j] == 0)
+                            {
+                                //Данный элемент минимальный и тариф не заполнен
+                                if (Tariff[0, j] >= Tariff[i, 0])
+                                {
+                                    Plan[i, j] = Tariff[i, 0];
+                                    Tariff[0, j] -= Tariff[i, 0];
+                                    Tariff[i, 0] -= Tariff[i, 0];
+                                }
+                                else if (Tariff[0, j] < Tariff[i, 0])
+                                {
+                                    Plan[i, j] = Tariff[0, j];
+                                    Tariff[i, 0] -= Tariff[0, j];
+                                    Tariff[0, j] -= Tariff[0, j];
+                                }
+                                Check[i, j] = 1;
+                            }
+                        }
+                    }
+                    SumCheck = 0;
+                    //Проверка на количество заполненных элементов
+                    for (int i = 1; i < Check.GetLength(0); i++)
+                    {
+                        for (int j = 1; j < Check.GetLength(1); j++)
+                        {
+                            SumCheck += Check[i, j];
+                        }
+                    }
+                }
+                //Ответ
+                Console.WriteLine("Тарифый план:");
+                for (int i = 1; i < Plan.GetLength(0); i++)
+                {
+                    for (int j = 1; j < Plan.GetLength(1); j++)
+                    {
+                        Console.Write(Plan[i, j] + " \t");
+                        L = L + (Plan[i, j] * Tariff[i, j]);
+                        if (Plan[i, j] != 0)
+                        {
+                            countV++;
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (V == countV)
+                {
+                    Console.WriteLine("Невырожденная задача!");
+                }
+                else
+                {
+                    Console.WriteLine("Вырожденная задача!");
+                }
+                Console.WriteLine($"L(X) = {L}");
+            }
+        }
+
+        static void Metod2()
         {
             int N, M;
             Console.WriteLine("Введите количество склада:");
@@ -171,6 +330,29 @@ namespace ВходнойКонтроль
                 }
                 Console.WriteLine($"L(X) = {L}");
             } 
+        }
+        static void Main()
+        {
+            int n, end;
+            do
+            {
+                Console.WriteLine("Выберите каким методом решить транспортную задачу: ");
+                Console.WriteLine("1. Метод северно-западного угла");
+                Console.WriteLine("2. Метод минимального элемента");
+                Console.WriteLine("3. Метод двойного предпочтения");
+                Console.WriteLine("4. Метод Фогеля");
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    //case 1: Zadanie1(); break;
+                    case 2: Metod2(); break;
+                    //case 3: Zadanie3(); break;
+                    //case 4: Zadanie4(); break;
+                    default: Console.WriteLine("Такого задания нет"); break;
+                }
+                Console.WriteLine("Вы хотите выбрать другой метод? (1 - да, 0 - нет).");
+                end = Convert.ToInt32(Console.ReadLine());
+            } while (end > 0);
         }
     }
 }
